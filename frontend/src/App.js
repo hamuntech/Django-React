7c638e1673f7cc26e81963cd68e20afa90dec31a
Hamuntech <hamuntech@gmail.com>
2022-05-11 21:47:38 -0400

    add signup and login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/App.js
+import ReactDOM from "react-dom/client";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login.js"
+import Dashboard from "./components/dashboard/Dashboard";
+import Root from "./Root"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <Root>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route exact path="/" element={<Home />} />
+    </Routes>
+  </Root>
+);
 }
-
-export default App;
+//https://reactrouter.com/docs/en/v6/getting-started/tutorial
